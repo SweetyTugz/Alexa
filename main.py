1fc85635498625b1cf145b640b0cf39ffaddb97b
SweetyTugz <sweettugz@gmail.com>
2021-10-05 12:19:38 +0100

    GUI is Ready

--- main.py
+++ main.py
@@ -21,4 +21,10 @@ chat.place(x=6,y=6,height=305,width=370)
 
 message=Text(root,bg='black',width=30,height=4)
 message.place(x=128,y=400,height=88,width=260)
+
+Button=Button(root,text='send',bg='blue',activebackround='light blue',width=12,height=5,font='Arial')
+Button.place(x=6,y=400,height=88,width=120)
+
+scrollbar=Scrollbar(root,command=chat.yview())
+scrollbar.place(x=375,y=5,height=385)
 root.mainloop()
